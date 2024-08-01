c00f83f2e37303fc1e16aa4c3bf191ed7c74bd47
oliver Legg <leggo646@gmail.com>
2024-08-01 18:53:42 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,110 @@
-import Image from "next/image";
+"use client";
+import ProjectCard, { Project } from "@/components/project-card";
+import { Button } from "@/components/ui/button";
+import {
+  faAngular,
+  faCss3,
+  faGoogle,
+  faHtml5,
+  faJava,
+  faJs,
+  faLinkedin,
+  faReact,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 
 export default function Home() {
+  const projects: Project[] = [
+    {
+      title: "Flocking simulator (boids algorithm)",
+      description:
+        "A visual display that demonstrates how three simple rules can lead to flocking behaviour. Have a play with the coeficients to see how each one plays their part.",
+      imageUrl: "/boids.png",
+      siteUrl: "https://leggit.github.io/boids/",
+      repoUrl: "https://github.com/Leggit/boids",
+      icons: [faHtml5, faJs, faCss3],
+    },
+    {
+      title: "Algebro",
+      description:
+        "At first glance, its just a calculator...but this calculator shows its workings - view the binary tree to discover how computers interpret mathematic expressions.",
+      imageUrl: "/algebro.png",
+      siteUrl: "https://algebro.netlify.app/calculator",
+      repoUrl: "https://github.com/Leggit/Algebro",
+      icons: [faJava, faAngular, faGoogle],
+    },
+    {
+      title: "geovis-lite",
+      description:
+        "A quick and dirty way of visualising geometries supplied in GeoJSON or WKT format",
+      imageUrl: "/geovislite.png",
+      siteUrl: "https://leggit.github.io/geovis-lite/",
+      repoUrl: "https://github.com/Leggit/geovis-lite",
+      icons: [faReact],
+    },
+    {
+      title: "PID Simulator",
+      description:
+        "This app aims to visually demonstrate how a PID loop can keep a quadcopter level. You can have a play with the different coefficients that are used in the PID calculation to try and perfect the self levelling.",
+      imageUrl: "/pidsim.png",
+      siteUrl: "https://leggit.github.io/pid-simulator/",
+      repoUrl: "https://github.com/Leggit/pid-simulator",
+      icons: [faHtml5, faJs, faCss3],
+    },
+    {
+      title: "Naughts and Crosses (with an AI)",
+      description:
+        "The classic game, but you can make the board bigger, play with timers and play against an AI powered by a minmax alpha-beta pruning algorithm.",
+      imageUrl: "/xo.png",
+      siteUrl: "https://leggit.github.io/naughtsandcrosses/",
+      repoUrl: "https://github.com/Leggit/naughtsandcrosses",
+      icons: [faHtml5, faJs, faCss3],
+    },
+    {
+      title: "Timex - time booking system",
+      description:
+        "A frontend for a time entry system that a business could use to record time spent by their employees on differnt projects. It was originally plugged into a Supabase backend but this example is a backenless demo",
+      imageUrl: "/timex.png",
+      siteUrl: "https://timex-app.netlify.app",
+      repoUrl: "https://github.com/Leggit/timex-public",
+      icons: [faReact],
+    },
+  ];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+    <div className="px-0 md:px-2 lg:px-4 max-w-[1400px] xl:m-auto">
+      <div className="px-3 my-8">
+        <h1 className="text-7xl mb-2 font-bold">Hi, I'm Ollie.</h1>
+        <p className="max-w-[500px] md:max-w-[700px] px-1 text-lg mb-2">
+          I'm an enthusiastic results driven developer who loves learning about
+          new things and using technology to solve problems. Here's a few of the
+          projects I've been working on recently (mostly) for fun.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+        <p className="px-1 text-sm mb-4">
+          To find out more about what I do for work checkout my&nbsp;
+          <Button variant="link" className="m-0 p-0">
+            <Link
+              className="font-semibold"
+              href="https://www.linkedin.com/in/oliver-legg-33191b276/"
+              target="_blank"
+            >
+              LinkedIn <FontAwesomeIcon icon={faLinkedin} />
+            </Link>
+          </Button>
+        </p>
+        <hr className="max-w-[700px]" />
+      </div>
+      <div className="flex flex-row flex-wrap ">
+        {projects.map((project, index) => (
+          <div
+            key={index}
+            className="basis-1/1 sm:basis-1/2 md:basis-1/2 lg:basis-1/3 flex p-2"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+            <ProjectCard {...project} />
+          </div>
+        ))}
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
---
+++ components/project-card.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import React, { useState } from "react";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "./ui/card";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import { AspectRatio } from "@radix-ui/react-aspect-ratio";
+import Image from "next/image";
+import { Skeleton } from "./ui/skeleton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowUpRightFromSquare,
+  IconDefinition,
+} from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+export interface Project {
+  title: string;
+  description: string;
+  siteUrl: string;
+  repoUrl: string;
+  imageUrl: string;
+  icons: IconDefinition[];
+}
+
+function ProjectCard({
+  title,
+  description,
+  siteUrl,
+  repoUrl,
+  imageUrl,
+  icons,
+}: Project) {
+  const [loadingImg, setLoadingImg] = useState(true);
+
+  return (
+    <Card className="max-w-[600px] flex-1 flex flex-col">
+      <CardHeader className="flex-grow">
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="mb-2">
+          {icons.map((icon, index) => (
+            <FontAwesomeIcon className="text-lg mr-2" key={index} icon={icon} />
+          ))}
+        </div>
+        <Link target="_blank" href={siteUrl}>
+          <div className="w-[100%]">
+            <AspectRatio ratio={4 / 3}>
+              {loadingImg && <Skeleton className="w-[100%] h-[100%]" />}
+              <Image
+                src={imageUrl}
+                onLoadingComplete={() => setLoadingImg(false)}
+                fill
+                alt="Image"
+                className="rounded-md object-cover hover:scale-105 transition ease-in-out"
+              />
+            </AspectRatio>
+          </div>
+        </Link>
+      </CardContent>
+      <CardFooter>
+        <Button size="sm" className="me-2" asChild>
+          <Link target="_blank" href={siteUrl}>
+            Visit site &nbsp;
+            <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+          </Link>
+        </Button>
+        <Button size="sm">
+          <Link target="_blank" href={repoUrl}>
+            View code&nbsp;
+            <FontAwesomeIcon className="text-lg" icon={faGithub} />
+          </Link>
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
+
+export default ProjectCard;
