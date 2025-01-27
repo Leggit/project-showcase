e27ea9da537b0c1cb9895816cc175cbfce21ba76
oliver Legg <leggo646@gmail.com>
2025-01-27 21:08:23 +0000

    Forgot about the apostrephe thing

--- components/name-header.tsx
+++ components/name-header.tsx
@@ -2,7 +2,7 @@ function NameHeader() {
   return (
     <h1 className="text-7xl">
       Hi,
-      <div className="flex">I'm Ollie.</div>
+      <div className="flex">{"I'm Ollie."}</div>
     </h1>
   );
 }
