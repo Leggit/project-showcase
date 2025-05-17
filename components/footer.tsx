6372346f5e52408783622c1dcdb9a9aac9fc2fb4
oliver Legg <leggo646@gmail.com>
2025-05-17 14:47:56 +0100

    Smaller footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,11 +3,11 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="bg-secondary min-h-[200px] flex justify-center flex-col">
+    <footer className="bg-secondary min-h-[100px] flex justify-center flex-col">
       <div className="p-2 md:px-2 lg:px-4 max-w-[1400px] xl:m-auto h-full">
         <div className="flex flex-col justify-center flex-grow flex-1">
           <div className="text-center font-light text-sm">
-            <div className="mt-5">
+            <div className="mt-3">
               <em>
                 This site was made with Next.js and Tailwind CSS - check out the
                 source code{" "}
