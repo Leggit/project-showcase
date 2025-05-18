85772b16fdc64f4390e7c7985ecea46e3e572180
oliver Legg <leggo646@gmail.com>
2025-05-18 19:52:05 +0100

    Minor backtrack on the title of the main page

--- components/name-header.tsx
+++ components/name-header.tsx
@@ -1,5 +1,9 @@
 function NameHeader() {
-  return <h1 className="text-7xl">{"Hi, I'm Ollie."}</h1>;
+  return (
+    <h1 className="text-7xl">
+      Hi, <div className="block md:inline">{"I'm Ollie."}</div>
+    </h1>
+  );
 }
 
 export default NameHeader;
